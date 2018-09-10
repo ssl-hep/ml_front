1a5d292573d33dce9311f824cfd25dad15e63c50
Ilija Vukotic <ivukotic@cern.ch>
2018-09-10 16:12:22 -0500

    deploying maniac

--- kube/maniac-ml/start.bat
+++ kube/maniac-ml/start.bat
@@ -1,12 +1,13 @@
 kubectl create -f namespace.yaml
 kubectl label nodes ml.usatlas.org mlnode=true
 
+
+
 kubectl create -f service_account.yaml
 
 kubectl create secret -n maniac-ml generic cert-secret --from-file=key=secrets/certificates/maniac-ml.pem --from-file=cert=secrets/certificates/maniac-ml.cer
 kubectl create secret -n maniac-ml generic globus-secret --from-file=gconf=secrets/globus-config.json
 kubectl create secret -n maniac-ml generic config --from-file=mlconf=secrets/config.json
+kubectl create secret -n maniac-ml generic mg-config --from-file=mgconf=secrets/mg-config.json
 
-
-cd ..
 kubectl create -f frontend.yaml
--- kube/ml-usatlas-org/start.bat
+++ kube/ml-usatlas-org/start.bat
@@ -8,5 +8,6 @@ kubectl create -f service_account.yaml
 kubectl create secret -n ml-usatlas-org generic cert-secret --from-file=key=secrets/certificates/ml-front.key.pem --from-file=cert=secrets/certificates/ml-front.cert.cer
 kubectl create secret -n ml-usatlas-org generic globus-secret --from-file=gconf=secrets/globus-config.json
 kubectl create secret -n ml-usatlas-org generic config --from-file=mlconf=secrets/config.json
+kubectl create secret -n ml-usatlas-org generic mg-config --from-file=mgconf=secrets/mg-config.json
 
 kubectl create -f frontend.yaml

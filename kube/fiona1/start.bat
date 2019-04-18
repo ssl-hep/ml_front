8be3e41e14e4ae80276294a506fbb419395a1e30
Ilija Vukotic <ivukotic@cern.ch>
2019-04-18 16:30:38 -0500

    adding fiona1

---
+++ kube/fiona1/start.bat
@@ -0,0 +1,14 @@
+kubectl create -f namespace.yaml
+kubectl label nodes ml.usatlas.org mlnode=true
+
+kubectl create -f ml_public.yaml
+
+kubectl create -f service_account.yaml
+
+kubectl delete secret -n ml-usatlas-org cert-secret
+kubectl create secret -n ml-usatlas-org generic cert-secret --from-file=key=secrets/certificates/ml-front.key.pem --from-file=cert=secrets/certificates/ml-front.cert.cer
+kubectl create secret -n ml-usatlas-org generic globus-secret --from-file=gconf=secrets/globus-config.json
+kubectl create secret -n ml-usatlas-org generic config --from-file=mlconf=secrets/config.json
+kubectl create secret -n ml-usatlas-org generic mg-config --from-file=mgconf=secrets/mg-config.json
+
+kubectl create -f frontend.yaml

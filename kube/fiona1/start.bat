d079c710199d52d773951a51985971430a829d11
Ilija Vukotic <ivukotic@cern.ch>
2019-07-10 12:33:46 -0500

    fiona1 move

--- kube/fiona1/start.bat
+++ kube/fiona1/start.bat
@@ -6,9 +6,9 @@ kubectl create -f ml_public.yaml
 kubectl create -f service_account.yaml
 
 
-kubectl create secret tls -n ml-usatlas-org ml-usatlas-org-tls --key secrets/certificates/ml-front.key.pem --cert secrets/certificates/ml-front.cert.cer
-kubectl create secret -n ml-usatlas-org generic globus-secret --from-file=gconf=secrets/globus-config.json
-kubectl create secret -n ml-usatlas-org generic config --from-file=mlconf=secrets/config.json
-kubectl create secret -n ml-usatlas-org generic mg-config --from-file=mgconf=secrets/mg-config.json
+kubectl create secret tls -n fiona1 fiona1-tls --key secrets/certificates/ml-front.key.pem --cert secrets/certificates/ml-front.cert.cer
+kubectl create secret -n fiona1 generic globus-secret --from-file=gconf=secrets/globus-config.json
+kubectl create secret -n fiona1 generic config --from-file=mlconf=secrets/config.json
+kubectl create secret -n fiona1 generic mg-config --from-file=mgconf=secrets/mg-config.json
 
 kubectl create -f frontend.yaml

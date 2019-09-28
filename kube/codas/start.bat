10e73d5b4cbb8e0719b43f47027eddae4350054d
Ilija Vukotic <ivukotic@cern.ch>
2019-09-28 15:07:48 +0200

    full move to cert manager

--- kube/codas/start.bat
+++ kube/codas/start.bat
@@ -3,7 +3,8 @@ kubectl label nodes ml.usatlas.org mlnode=true
 
 kubectl create -f service_account.yaml
 
-kubectl create secret tls -n codas codas-tls --key secrets/certificates/codas.pem --cert secrets/certificates/codas.cer
+REM Not needed as this is done by certmanager.
+REM kubectl create secret tls -n codas codas-tls --key secrets/certificates/codas.pem --cert secrets/certificates/codas.cer
 kubectl create secret -n codas generic globus-secret --from-file=gconf=secrets/globus-config.json
 kubectl create secret -n codas generic config --from-file=mlconf=secrets/config.json
 kubectl create secret -n codas generic mg-config --from-file=mgconf=secrets/mg-config.json
--- kube/ml-usatlas-org/start.bat
+++ kube/ml-usatlas-org/start.bat
@@ -5,8 +5,9 @@ kubectl create -f ml_public.yaml
 
 kubectl create -f service_account.yaml
 
-kubectl delete secret -n ml-usatlas-org ml-usatlas-org-tls
-kubectl create secret tls -n ml-usatlas-org ml-usatlas-org-tls --key secrets/certificates/ml-front.key.pem --cert secrets/certificates/ml-front.cert.cer
+REM Not needed as this is done by certmanager
+REM kubectl delete secret -n ml-usatlas-org ml-usatlas-org-tls
+REM kubectl create secret tls -n ml-usatlas-org ml-usatlas-org-tls --key secrets/certificates/ml-front.key.pem --cert secrets/certificates/ml-front.cert.cer
 
 kubectl create secret -n ml-usatlas-org generic globus-secret --from-file=gconf=secrets/globus-config.json
 

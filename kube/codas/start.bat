df11fdc7f9be3839dee650849d6b465c4c212034
Ilija Vukotic <ivukotic@cern.ch>
2020-09-29 09:45:01 -0500

    rew

--- kube/codas/start.bat
+++ kube/codas/start.bat
@@ -6,8 +6,11 @@ kubectl create -f service_account.yaml
 REM Not needed as this is done by certmanager.
 REM kubectl create secret tls -n codas codas-tls --key secrets/certificates/codas.pem --cert secrets/certificates/codas.cer
 kubectl create secret -n codas generic globus-secret --from-file=gconf=secrets/globus-config.json
+
+kubectl delete secret -n codas config
 kubectl create secret -n codas generic config --from-file=mlconf=secrets/config.json
 kubectl create secret -n codas generic mg-config --from-file=mgconf=secrets/mg-config.json
 
+kubectl delete -f frontend.yaml
 kubectl create -f frontend.yaml
 
